fc5e402a9bd1a1efc4c195535e698398032a458b
Mosscow00 <141783259+Mosscow00@users.noreply.github.com>
2024-09-16 16:40:12 +0300

    Update اخرج.js

--- plugins/اخرج.js
+++ plugins/اخرج.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, '*وقتكو خلص يولاد الـ متناكةةة*','*حرب اليكس*') 
+await conn.reply(id, '*وقتكو خلص معايا يولاد الـ متناكةةةةة*') 
 await conn.groupLeave(id)}
 handler.command = /^(اخرج|اطلع|غادر|خروج)$/i
 handler.group = true
