cdb37dc11c2c9d93c5361f98343c5c4e79d9a704
Mosscow00 <141783259+Mosscow00@users.noreply.github.com>
2024-09-16 16:20:55 +0300

    Update Chatbot.js

--- plugins/Chatbot.js
+++ plugins/Chatbot.js
@@ -4,25 +4,11 @@ let handler = m => m;
  let chat = global.db.data.chats[m.chat]; 
  let responses; 
  if (/^حرب$/i.test(m.text)) { 
- responses = [ 
- 'نعم؟'
- ]; 
+ responses = ['نعم؟']; 
  } else if (/^كسم البوت|بوت$/i.test(m.text)) { 
-     responses = [ 
- 'أسمى بوت حرب 😁'
-     ]; 
+     responses = [ 'أسمى بوت حرب 😁']; 
  }else if (/^تست$/i.test(m.text)) { 
-     responses = [ 
-       'البوت شغال تمام',  
- 
- } else if (/^بوت عرص$/i.test(m.text)) { 
-     responses = [ 
- 'هو نا كنت بعرص ع امك ولا اي؟'
-     ];     
- } else if (/^كسمك|كسمك بوت|يبنلشرموطة$/i.test(m.text)) { 
-     responses = [ 
-'كسمك عشان البوت 😁'
-     ]; 
+     responses = [ 'البوت شغال تمام'],  
  }
  if (responses) { 
  let randomIndex = Math.floor(Math.random() * responses.length); 
