27c8a87618035224b258231ba9cbabc8af3e436f
Mosscow00 <141783259+Mosscow00@users.noreply.github.com>
2024-09-16 12:30:35 +0300

    Update ابلاغ.js

--- plugins/ابلاغ.js
+++ plugins/ابلاغ.js
@@ -3,7 +3,7 @@ if (!text) throw `*[❗] ادخل مشكلتك أو بلاغك*\n\n*مثال:*\n
 if (text.length < 10) throw `*[❗] البلاغ لا يقل عن عشرة احرف*`
 if (text.length > 1000) throw `*[❗] البلاغ لا يزيد عن الف حرف*`
 let teks = `*❒═════[إبلاغ مهم]═════❒*\n*┬*\n*├❧ الرقم:* wa.me/${m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ البلاغ:* ${text}\n*┴*`
-conn.reply('201012531172@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, { contextInfo: { mentionedJid: [m.sender] }})
+conn.reply('201210307869@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, { contextInfo: { mentionedJid: [m.sender] }})
 conn.reply('@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, { contextInfo: { mentionedJid: [m.sender] }})
 m.reply(`*[ ✔️ ] تم إبلاغ مطوري , أنتظر حتى يرى البلاغ الخاص بك
 handler.help = ['reporte', 'request'].map(v => v + ' <teks>')
