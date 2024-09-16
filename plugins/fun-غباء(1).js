49555b5ff1990274df1a03a6e2ff94ffef85c5f1
Mosscow00 <141783259+Mosscow00@users.noreply.github.com>
2024-09-16 21:20:44 +0300

    Update fun-غباء(1).js

--- plugins/fun-غباء(1).js
+++ plugins/fun-غباء(1).js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command, text }) => {
 let stupidity = `*نسبة الغباء*
-*نسبة غباء ${text}هي* *${Math.floor(Math.random() * 100)}%* *من 100%*
+*نسبة غبائك ${text}هي* *${Math.floor(Math.random() * 100)}%* *من 100%*
 *ربنا يهديكم*
 `.trim()
 m.reply(stupidity, null, { mentions: conn.parseMention(stupidity) })}
