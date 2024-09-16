3d2f40d3f866436419fb5e3b191e4c500ddf3813
Mosscow00 <141783259+Mosscow00@users.noreply.github.com>
2024-09-16 21:08:31 +0300

    Update fun-ذكاء(1).js

--- plugins/fun-ذكاء(1).js
+++ plugins/fun-ذكاء(1).js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
 let intelligence = `*💡 نسبة الذكاء 💡*
 *نسبة ذكاء ${text} 💡هي* *${Math.floor(Math.random() * 100)}%* *من 100%*
-*ربنا يهديكم💙☁️*
+*ربنا يهديكم*
 `.trim()
 m.reply(intelligence, null, { mentions: conn.parseMention(intelligence) })}
 handler.help = ['intelligence']
